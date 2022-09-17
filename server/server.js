302f733ea3baaaeaa84b5376ad5f5021edb574ca
JP Moregain <j.p.moregain15@gmail.com>
2022-09-17 17:51:41 -0400

    Begin building out react components for main application, refactor redux code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request')
+// const request = require('request')
 const app = express();
 const port = 3000;
 
@@ -12,27 +12,12 @@ const mockResponse = {
   bar: 'foo'
 };
 
-const options = {
-  url: 'https://api.api-ninjas.com/v1/exercises?muscle=chest',
-  headers: {
-    'X-Api-Key': 'yPvp7DZtvxsNwvYebl/CbA==Qp9XuU0upuR0x5BD'
-  }
-}
-
-function callback(error, req, res) {
-  if(error) return console.error('Request failed:', error);
-  else if(req.statusCode != 200) return console.error('Error:', req.statusCode, res.toString('utf8'));
-  console.log(res)
-  }
-
-request(options, callback)
-
 app.get('/api', (req, res) => {
   res.send(mockResponse);
 });
 
 app.get('/', apiController.chest, (req, res) => {
- res.status(200).send('Hello World from the server!');
+ res.status(200).json(res.locals.chest);
 });
 
 
@@ -40,3 +25,4 @@ app.get('/', apiController.chest, (req, res) => {
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
 });
+
